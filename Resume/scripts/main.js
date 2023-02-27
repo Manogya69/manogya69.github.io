25f73646297e2e8bb3defc73db2ac93c753edb18
manogya69 <manogya57@gmail.com>
2023-02-27 23:25:24 +0545

    Made  Code Cleaner

--- Resume/scripts/main.js
+++ Resume/scripts/main.js
@@ -1,7 +1,18 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+  anchorPlacement: "top-left",
+  duration: 1000,
 });
 
-// Add your javascript here
+const toggleEl = document.querySelector(".toggle");
+
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+});
